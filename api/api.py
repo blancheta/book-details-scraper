90f994edd39c72ffae2cbf0744defbc2ac22f495
Aristofane1 <aristofanesmithloko@gmail.com>
2021-08-22 05:02:28 +0100

    error message and test if isbn is valid

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+"""Book scraping api"""
 import logging
 
 from flask import Flask, jsonify, request
@@ -9,20 +10,47 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
+class BadRequestException(Exception):
+    """bad request inherit exception class"""
+
+    pass
+
+
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1> copy this link and add ibsn code to get book details </h1> <p>http://127.0.0.1:5000/api/v1/book-details/?ibsn=</p>"
+    """home page"""
+    return (
+        "<h1> copy this link and add ibsn code to get book details </h1> "
+        "<p>http://127.0.0.1:5000/api/v1/book-details/?ibsn=</p> "
+    )
 
 
 @app.route("/api/v1/book-details/", methods=["GET"])
 def get_book_details():
+    """get book details"""
     # test and log
     url_test = "https://www.paperbackswap.com/book/browser.php?k=9782746040885"
-    find_test = BeautifulSoup(requests.get(url_test).text, "html.parser").find("span", itemprop="isbn")
+    find_test = BeautifulSoup(requests.get(url_test).text, "html.parser").find(
+        "span", itemprop="isbn"
+    )
+    # if the website code has been modified, then we intercept our code and send an error
     if find_test is None:
-        logging.critical('ALERT: the website code has modified')
-
+        logging.error("ALERT: the website code has modified")
+        return (
+            jsonify(
+                error={
+                    "code": "target_website_modified_error",
+                    "message": "Target website has been modified",
+                }
+            ),
+            422,
+        )
+    # we get the ibsn, we check and we return a message according to the result
     ibsn = request.args.get("ibsn")
+    if ibsn == "":
+        print("ibsn")
+        raise BadRequestException()
+
     # connect to website
     url = "https://www.paperbackswap.com/book/browser.php?k=" + ibsn
     html = requests.get(url)
@@ -57,7 +85,7 @@ def get_book_details():
             200,
         )
     except Exception as e:
-        print(e)
+        logging.error(e)
         return (
             jsonify(
                 error="<isbn> is incorrect",
